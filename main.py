bb42e23a8ca8ed9b625f09da20cbbec362e82cac
Franz from Melany <me@francescopaissan.it>
2020-05-29 13:32:49 +0200

    First code commit; Network implementation in PyTorch

---
+++ main.py
@@ -0,0 +1,13 @@
+import src.model.model_trainer as model_trainer
+import src.parameters as par
+
+import numpy as np
+import torchvision
+import torch
+
+#Net debugging
+X = []
+X.append(torch.tensor(np.ones(shape=(1, par.AUDIO_C, par.AUDIO_H, par.AUDIO_W))))
+X.append(torch.tensor(np.ones(shape=(1, par.VIDEO_C, par.VIDEO_H, par.VIDEO_W))))
+
+model_trainer.train(X, None)
---
+++ src/model/model_trainer.py
@@ -0,0 +1,38 @@
+import src.model.vision_model as vision_model
+import src.model.audio_model as audio_model
+from .. import parameters as par
+
+import torch.optim as optim
+import torch.nn as nn
+import numpy as np
+import torchvision
+import torch
+
+if par.MODEL_TYPE == 'default':
+    class Net(nn.Module):
+        def __init__(self, audioNet, visionNet):
+            super(Net, self).__init__()
+            self.visionNet = visionNet
+            self.audioNet = audioNet
+            self.lin1 = nn.Linear(1024, 128)
+            self.lin2 = nn.Linear(128, 2)
+
+            self.relu = nn.ReLU()
+            self.soft = nn.Softmax()
+
+        def forward(self, audioX, visionX):
+            x = torch.cat((self.visionNet.forward(visionX.double()), self.audioNet.forward(audioX.double())), 0)
+            x = self.lin1(x)
+            x = self.relu(x)
+            x = self.lin2(x)
+
+            return self.soft(x)
+
+def train(X, y, batch_size=64, lr=1e-4, weigth_decay=1e-5):
+    audioNet = audio_model.Net().double()
+    visionNet = vision_model.Net().double()
+
+    
+
+    avcNet = Net(audioNet, visionNet).double()
+    print("AVC Net output shape: ", avcNet.forward(X[0], X[1]).shape)
