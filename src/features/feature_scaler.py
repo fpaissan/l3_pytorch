22436b70c9b3da19f6d3fb2ff5131d401cc84608
francescopaissan <me@francescopaissan.it>
2020-08-10 14:01:49 +0200

    dev on scaler #2

--- src/features/feature_scaler.py
+++ src/features/feature_scaler.py
@@ -2,14 +2,12 @@ from src.model.avc_trainer import avcNet_generator
 import src.features.features_parameters as feat_p
 from src.features.utils import *
 
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import torch
 
 from tqdm import tqdm
 import argparse
 import pickle
-import gzip
-import sys
-import os
 
 import glob
 
@@ -39,41 +37,14 @@ def extract_scalers(data_dir, output_dir, limit=-1):
 
     feat_array = np.asarray(feat_list)
 
-    print(len(file_list), feat_array.shape)
+    minmaxScaler = MinMaxScaler()
+    stdScaler = StandardScaler()
 
-    #     audio_path = os.path.join(data_dir, file_list[i])
-    #
-    #     audioSignal, sr = sf.read(audio_path, dtype='int16', always_2d=True)
-    #     audioSignal = audioSignal.mean(axis=-1).astype('int16')
-    #     audioSignal = resampy.resample(audioSignal, sr, 48000)
-    #
-    #     if (not is_openl3):
-    #         spectrograms = audio_feat(audioSignal, 48000, is_openl3=is_openl3)
-    #
-    #     if (not is_openl3):
-    #         spectrograms = torch.from_numpy(spectrograms)
-    #         spectrograms = spectrograms.to("cuda")
-    #
-    #         features = model.forward(spectrograms)
-    #         features = features.cpu().numpy()
-    #
-    #     basename = file_list[i].split('.')[0]
-    #     class_label = int(basename.split('-')[-1])
-    #
-    #     if (not is_openl3):
-    #         with open(os.path.join(output_dir, 'fold' + basename.split('-')[0], basename + '.pkl'), 'wb') as f:
-    #             pickle.dump((features, class_label), f)
-    #
-    #     if (is_openl3):
-    #         # Creating audio list to avoid GPU memory overflow while inferencing
-    #         audioSignal_folds[basename.split('-')[0]].append(audioSignal)
-    #         classLabel_folds[basename.split('-')[0]].append(class_label)
-    #
-    # for fold in range(5):  # iterate in folds
-    #     spectrograms_openL3 = audio_feat(audioSignal_folds[str(fold + 1)], 48000, is_openl3=is_openl3)
-    #     for x, sample in enumerate(spectrograms_openL3):
-    #         with open(os.path.join(output_dir, 'fold{}'.format(fold + 1), '{}.pkl'.format(x)), 'wb') as f:
-    #             pickle.dump((sample, classLabel_folds[str(fold + 1)][x]), f)
+    feat_array = minmaxScaler.fit_transform(feat_array)
+    stdScaler.fit(feat_array)
+
+    with open(os.path.join(output_dir, 'scalers.pkl'), 'wb') as fp:
+        pickle.dump((minmaxScaler, stdScaler), fp)
 
 if __name__ == "__main__":
     args = parse_arguments()
